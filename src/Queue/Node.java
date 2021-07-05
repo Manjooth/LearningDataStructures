83a0c739a9d32421ef94121f77b2898c3a46bde0
Manjooth <manjoothkler@gmail.com>
2021-07-05 20:16:20 +0100

    implementing queue using linked lists

---
+++ src/Queue/Node.java
@@ -0,0 +1,32 @@
+package Queue;
+
+public class Node<T> {
+
+    private T data;
+    private Node<T> nextNode;
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{data=" + data + '}';
+    }
+}
